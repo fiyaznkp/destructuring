61edbbda5b96b833b8b29802a2e78f639bbffe26
fiyaznkp <fiyaznkp@gmail.com>
2023-10-31 10:12:59 +0530

    array destructuring

---
+++ App.js
@@ -0,0 +1,17 @@
+// array destructuring ; it means unpacking values from array to a bunch of variables
+
+// var arr = ["kerala","malayalam","350"]
+
+// var state = arr[0] here if we console 0,1 the resul will be malayalam 
+
+// console.log(state)
+
+// here the above one is the normal way of assingning array elements to a variable
+
+// here is the steps for array destructuring
+
+const arr = ["fiyaz","koduvally","25"]
+
+const [name,place,age] = arr
+
+console.log(name)
