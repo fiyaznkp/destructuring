ffac2e71907d42fe83a362749448ee0df20502b9
fiyaznkp <fiyaznkp@gmail.com>
2023-11-02 10:52:21 +0530

    js basics

--- App.js
+++ App.js
@@ -10,8 +10,73 @@
 
 // here is the steps for array destructuring
 
-const arr = ["fiyaz","koduvally","25"]
+// const arr = ["fiyaz","koduvally","25"]
 
-const [name,place,age] = arr
+// const [name,place,age] = arr
 
-console.log(name)
+// console.log(name)
+
+// const numbers = [1,2,3,4,5]
+
+// const [one,two,three,four,five] = numbers
+
+// console.log(one)
+
+//destructuring means extracting values from arrays and objects and assigning them to variables.
+
+// it is example for nested array
+
+// const arr = [1,2,3,4,5,[6,7,8,9]]
+
+// const [one,two,three,four,five,[six,seven,eight,nine]] = arr
+
+// console.log(nine)
+
+//object destructuring 
+
+// const obj = {
+//     name:"fiyaz",
+//     age:25,
+//     place:"koduvally"
+// }
+// const {name,age,place} = obj
+
+// console.log(name)
+
+// const obj = {
+//     name:"fiyaz",
+//     place:"koduvally",
+//     age:25
+// }
+
+// const {name,place,age} = obj
+
+// console.log(name,age)
+
+// const obj = {
+//     name:"fiyaz",
+//     age:25,
+
+//    more:{
+//     place:"koduvally"
+
+//    }
+    
+// }
+// const {name,age, more:{place}} = obj
+
+// console.log(place)
+
+const obj = {
+    name:"fiyaz",
+    age:25,
+
+    address:{
+        place:"koduvally"
+    }
+}
+
+
+const {name,age, address:{place}} = obj
+
+console.log(place)
---
+++ array.js
@@ -0,0 +1,44 @@
+// const array =[1,2,3,4,5]
+
+// function filter(x) {
+//   if(x % 2 != 0) {
+//      return x
+//   }
+// }
+
+// const result = array.filter(filter)
+
+// console.log(result)
+
+
+// const arr=[1,2,3,3]
+
+// const result = arr.length
+
+// console.log(result)
+
+// const years=[1949,1950,1951,1952]
+
+// function YearGreater (year) {
+//     return year >1950
+// }
+
+// const result= years.filter(YearGreater)
+
+// console.log(result)
+
+// const years =[2010,2011,2012]
+
+// function yearsLessthan (year) {
+//     return year < 2011
+// }
+// const result = years.filter(yearsLessthan)
+
+// console.log(result)
+
+// const years = [2023,2022,2021]
+
+// const result = years.filter((year)=>year<2022)
+
+// console.log(result)
+
